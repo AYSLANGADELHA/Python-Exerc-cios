145f1e5a46ba2ab53909a7c4c5a74e5b8bd42708
AYSLANGADELHA <ayslan.gadelha2004@gmail.com>
2024-06-05 14:11:58 -0300

    Create EXERCÍCIO 1.py

---
+++ EXERCÍCIO 1.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome:")
+print("É um prazer te conhecer,{}!".format(nome))
+n1=int(input('Digite um número:'))
+n2=int(input('Digite mais um número:'))
+s=n1+n2
+print(f'A soma vale {s}')
+
+
