63dfc99818008e4615933900046b7123408f74a3
AYSLANGADELHA <ayslan.gadelha2004@gmail.com>
2024-06-05 14:22:33 -0300

    EXERCÍCIOS

---
+++ EXERCÍCIO 7.py
@@ -0,0 +1,12 @@
+#Exercício 7 - operaçãoes aritméticas
+
+a1 = float(input('Qual foi a nota de João em matemática no primeiro semestre de 2024?  '))
+a2 = float(input('E no segundo semestre?'))
+
+
+x = a1 + a2
+y = (a1+a2) /2
+
+print('A soma das notas de Joãozinho é {} , A média das notas é {:.1f}'.format(x,y))
+
+
